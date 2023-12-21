18165caa89b75dbd569d139e4d86826b7310e886
MherKK <Mherkevorkian2@gmail.com>
2023-12-22 00:13:30 +0400

    fixed sign in error in rootlayout

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -7,7 +7,7 @@ const RootLayout = () => {
     <div>
         <Nav/>
         
-        {userData == 'empty' ? <p className='underline underline-offset-4 text-slate-gray font-montserrat border-coral-red pt-[100px] text-center text-sm p-2'>{'Save your favorite items by '}  
+        {userData == 'empty' || undefined ? <p className='underline underline-offset-4 text-slate-gray font-montserrat border-coral-red pt-[100px] text-center text-sm p-2'>{'Save your favorite items by '}  
           <Link to='/signin' className='text-coral-red underline '> Sign In</Link> {'/ '}  
           <Link to='/signup' className='text-coral-red underline'>Sign Up</Link>
         </p> : 
