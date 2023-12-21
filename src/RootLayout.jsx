788a62879df89e28df3d7a495bbb7930ebedae7a
MherKK <Mherkevorkian2@gmail.com>
2023-12-22 00:10:20 +0400

    changed the shopping bag card

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -12,7 +12,7 @@ const RootLayout = () => {
           <Link to='/signup' className='text-coral-red underline'>Sign Up</Link>
         </p> : 
         <div className='underline underline-offset-4 text-slate-gray font-montserrat border-coral-red pt-[100px] text-center text-sm p-2'>
-          {'Welcome to Nike Store ' + userData.name.slice(0,1).toUpperCase() + userData.name.slice(1,userData.name.length) + ' ,'} 
+          {'Welcome to Nike Store ' + userData?.name.slice(0,1).toUpperCase() + userData?.name.slice(1,userData?.name.length) + ' ,'} 
           <button onClick={() => {
             document.location.reload();
             localStorage.setItem('userData',JSON.stringify('empty'))
--- src/components/ShoppingBag.jsx
+++ src/components/ShoppingBag.jsx
@@ -3,7 +3,7 @@ import { Footer } from '../sections'
 import { shoe4 } from '../assets/images'
 
 const ShoppingBag = () => {
-    const [quantity,setQuantity] = useState(1);
+    let quantity = [1,2,3,4,5,6,7,8,9,10];
   return (
     <div>
         <div className='p-10 bg-black max-md:bg-white flex flex-col justify-center items-center'>
@@ -14,25 +14,26 @@ const ShoppingBag = () => {
                 </div>
                 <div>
                     <p className='text-[18px] text-coral-red font-bold'>$100.00</p>
-                    <div>
+                    <div className='flex flex-col gap-2'>
                         <p className='font-bold text-[20px]'>Air Jordan 1</p>
                         <p className='text-gray-400 '>Men's Shoes</p>
-                        <p className='text-gray-400  text-xl  '>Size  
-                            <select className='text-base  '>
-                                <option>EU 12</option>
-                                <option>EU 12.5</option>
-                                <option>EU 13</option>
+                        <div className='text-gray-400 flex items-start gap-2 max-sm:flex-col '>  
+                            <span className='text-base max-sm:text-[12px]'>Size 
+                                <select className='max-sm:text-[12px] text-base'>
+                                    <option>EU 12</option>
+                                    <option>EU 12.5</option>
+                                    <option>EU 13</option>
+                                </select>
+                            </span>
+                            <span className='text-base max-sm:text-[12px]'> Quantity  
+                            <select className='text-[12px] text-base'>
+                                {quantity.map((qnt) => {
+                                    return <option key={qnt}>{qnt}</option>
+                                })}
                             </select>
-
-                            <div className='text-base'> Quantity:  
-                                <button onClick={() => quantity > 1 && setQuantity(quantity - 1) } className='border max-sm:px-1 mx-2 px-2'> - </button>
-                                <input style={{WebkitAppearance:'none',margin:0}} className='max-sm:w-4 w-8 text-center border-none ' type='text' value={quantity}
-                                onChange={(e) => {
-                                    (Number.isInteger(+e.target.value) === true && +e.target.value >= 1 && +e.target.value <= 10 || e.target.value == '') && setQuantity(+e.target.value)
-                                } } />
-                                <button onClick={() => quantity < 10 && setQuantity(quantity + 1) } className='border mx-2 px-2 max-sm:px-1'> + </button>
-                            </div>
-                        </p>
+                            </span>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
